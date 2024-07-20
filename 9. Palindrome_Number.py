1583ad85a4a26972b8d40b577777627cb86ac0d5
brentverstreepen <brentverstreepen@hotmail.com>
2024-07-20 10:08:56 +0200

    update

--- 9. Palindrome_Number.py
+++ 9. Palindrome_Number.py
@@ -5,21 +5,20 @@
 # Output: true
 # Explanation: 121 reads as 121 from left to right and from right to left.
 # Example 2:
-#
 # Input: x = -121
 # Output: false
 # Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
 # Example 3:
-#
 # Input: x = 10
 # Output: false
 # Explanation: Reads 01 from right to left. Therefore, it is not a palindrome.
-
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        else:
+            return str(x) == str(x)[::-1]
 
-
-
-    isPalindrome(121, 121)
-    isPalindrome(-121, -121)
-    isPalindrome(10, 10)
+    print(isPalindrome(121, 121))
+    print(isPalindrome(-121, -121))
+    print(isPalindrome(10, 10))
