cdcf857c9a4de80ddcd753eb52ddc17f211139bd
brentverstreepen <brentverstreepen@hotmail.com>
2024-07-21 12:48:43 +0200

    update

--- 1. Two_Sum.py
+++ 1. Two_Sum.py
@@ -13,3 +13,16 @@
 # Input: nums = [3,3], target = 6
 # Output: [0,1]
 
+def twoSum(nums: list[int], target: int) -> list[int]:
+    seen = {}  # value : index
+    for i, n in enumerate(nums):
+        diff = target - n
+        if diff in seen:
+            return [seen[diff], i]
+        seen[n] = i
+    return []
+
+
+print(twoSum([2, 7, 11, 15], 9))
+print(twoSum([3, 2, 4], 6))
+print(twoSum([3, 3], 6))
