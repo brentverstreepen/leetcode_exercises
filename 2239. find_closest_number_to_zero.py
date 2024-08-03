5c2effe8e41ced5bc4d3e965a670f0b9de83993e
brentverstreepen <brentverstreepen@hotmail.com>
2024-08-03 10:11:51 +0200

    2239. find_closest_number_to_zero.py

--- 2239. find_closest_number_to_zero.py
+++ 2239. find_closest_number_to_zero.py
@@ -1,5 +1,6 @@
 # Given an integer array nums of size n, return the number with the value closest to 0 in nums.
 # If there are multiple answers, return the number with the largest value.
+#
 # Example 1:
 # Input: nums = [-4,-2,1,4,8]
 # Output: 1
@@ -10,6 +11,7 @@
 # The distance from 4 to 0 is |4| = 4.
 # The distance from 8 to 0 is |8| = 8.
 # Thus, the closest number to 0 in the array is 1.
+#
 # Example 2:
 # Input: nums = [2,-1,1]
 # Output: 1
