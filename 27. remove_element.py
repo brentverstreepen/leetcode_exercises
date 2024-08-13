3bf146f02073a366b7dade5985a5f125d8edcff4
brentverstreepen <brentverstreepen@hotmail.com>
2024-08-13 17:29:08 +0200

    27. remove_element.py

--- 27. remove_element.py
+++ 27. remove_element.py
@@ -18,10 +18,14 @@
 # Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
 # Note that the five elements can be returned in any order.
 # It does not matter what you leave beyond the returned k (hence they are underscores).
-def removeElement(nums: list[int]):
+def removeElement(nums: list[int], val: int):
+    insert_index = 0
+    for i in range(0, len(nums)):
+        if nums[i] != val:
+            nums[insert_index] = nums[i]
+            insert_index += 1
+    return insert_index
 
 
-
-
-print(removeElement([3,2,2,3]))
-print(removeElement([0,1,2,2,3,0,4,2]))
+print(removeElement([3, 2, 2, 3], 3))
+print(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2))
