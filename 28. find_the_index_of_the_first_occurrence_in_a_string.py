fb4dca0c1e18d1b44ddb1a3bcc8880ef21860f92
Brent <brentverstreepen@hotmail.com>
2024-08-17 12:03:46 +0200

    28. find_the_index_of_the_first_occurrence_in_a_string.py

--- 28. find_the_index_of_the_first_occurrence_in_a_string.py
+++ 28. find_the_index_of_the_first_occurrence_in_a_string.py
@@ -13,3 +13,20 @@
 # Explanation: "leeto" did not occur in "leetcode", so we return -1.
 
 def strStr(haystack: str, needle: str):
+    if needle == "":
+        return 0
+
+    # for i in range(len(haystack) + 1 - len(needle)):
+    #     if haystack[i: i + len(needle)] == needle:
+    #         return i
+    for i in range(len(haystack) + 1 - (len(needle))):
+        for j in range(len(needle)):
+            if haystack[i + j] != needle[j]:
+                break
+            if j == len(needle) - 1:
+                return i
+    return -1
+
+
+strStr("sadbutsad", "sad")
+strStr("leetcode", "leeto")
