53d246a1249825c656ea8aa5ff7a9e07d21d9214
brentverstreepen <brentverstreepen@hotmail.com>
2024-09-07 20:09:57 +0200

    67. add_binary.py

--- 67. add_binary.py
+++ 67. add_binary.py
@@ -7,3 +7,17 @@
 # Input: a = "1010", b = "1011"
 # Output: "10101"
 def addBinary(a: str, b: str) -> str:
+    a = int(a, 2)
+    b = int(b, 2)
+
+    while b:
+        without_carry = a ^ b
+        carry = (a & b) << 1
+        a = without_carry
+        b = carry
+
+    return bin(a)[2:]
+
+
+print(addBinary("11", "1"))
+print(addBinary("1010", "1011"))
