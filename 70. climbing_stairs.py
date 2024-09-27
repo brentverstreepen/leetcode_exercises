42293210deca9af38166f2144112fafd999b42db
Brent <brentverstreepen@hotmail.com>
2024-09-27 22:32:43 +0200

    70. climbing_stairs.py

--- 70. climbing_stairs.py
+++ 70. climbing_stairs.py
@@ -20,7 +20,14 @@ def climbStairs(n: int) -> int:
     if n == 2:
         return 2
 
-    return climbStairs(n - 2) + climbStairs(n - 1)
+    dp = [0] * n
+    dp[0] = 1
+    dp[1] = 2
+
+    for i in range(2, n):
+        dp[i] = dp[i - 2] + dp[i - 1]
+
+    return dp[n - 1]
 
 
 print(climbStairs(2))
